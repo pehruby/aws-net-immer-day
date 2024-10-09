54c326e9645749f973f11adadf9d5e7ad18c231d
Petr Hruby <petr.hruby@gmail.com>
2024-10-09 16:57:00 +0200

    README, comments

--- CML-prep/README.md
+++ CML-prep/README.md
@@ -4,3 +4,24 @@ https://www.youtube.com/watch?v=vzgUyO-GQio
 
 https://github.com/CiscoDevNet/cloud-cml/blob/main/documentation/AWS.md
 
+
+Group cmlterraform is created and also user cml_terraform which is member of the group
+Role s3-access-for-ec2 is created
+
+The following policies are created or used:
+- AmazonEC2FullAccess (predefined policy)
+-- assigned to group cmlterraform
+- AmazonSSMManagedInstanceCore (predefined policy)
+-- assigned to role s3-access-for-ec2
+- cml-s3-access
+-- customer managed (created) policy which enables access to s3 bucket named phcmlimages
+-- assigned to group cmlterraform and to role s3-access-for-ec2
+- pass-role
+-- inline policy which enables to pass role s3-access-for-ec2
+-- assigned to group cmlterraform
+
+
+Terraform runs under the user cmlterraform
+- it has access to EC2, to specific S3 bucket which contains installation files for CML and it can assume (pass) role s3-access-for-ec2 (this is used to pass this role to created EC2 instance so that it has access to the S3 bucket)
+
+Created EC2 instance has access to S3 bucket and is able to connect to SSM service thanks to role s3-access-for-ec2 which is passed to the EC2 instance
--- CML-prep/iam.tf
+++ CML-prep/iam.tf
@@ -1,52 +1,36 @@
+### Create groups
+
 # User group
 resource "aws_iam_group" "cmlterraform" {
   name = "cmlterraform"
   path = "/"
 }
 
-/*
-import {
-  to = aws_iam_group_policy.s3_access_policy
-  id = "group_of_mypolicy_name:mypolicy_name"
-}
-*/
+### Create user
 
-# IAM policy for user group cmlterraform
-# in video AmazonEC2FullAccess policy is also used as policy wich is passed to EC2 instance so that EC2 instance has  used
-# used to read from cmlterraform bucket where CML images are stored
+# Probably we will have to create access keys in GUI for the user ?
+resource "aws_iam_user" "cml_terraform" {
+  name = "cml_terraform"
+  path = "/"
 
-/*
-# This create inline policy, bellow attached policy is also used as policy wich is passed to EC2 instance so that EC2 instance has  used
-resource "aws_iam_group_policy" "s3_access_policy" {
-  name  = "s3_access_policy"
-  group = aws_iam_group.cmlterraform.name
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-        {
-            Sid = "VisualEditor0",
-            Effect = "Allow",
-            Action = [
-                "s3:PutObject",
-                "s3:GetObject",
-                "s3:ListBucket"
-            ],
-            Resource = [
-                "arn:aws:s3:::cmlterraform",
-                "arn:aws:s3:::cmlterraform/*"
-            ]
-        },
-    ]   
-  })
+  tags = {
+    tag-key = "cml_terraform"
+  }
 }
 
-*/
-# Policy that specifies that users in this group can pass the role to the EC2 instance
-# Will be attaced to user group
-# Policy restric the access to our S3 bucket only
+# associate the user with user group
+resource "aws_iam_user_group_membership" "cml_terraform" {
+  user = aws_iam_user.cml_terraform.name
+
+  groups = [
+    aws_iam_group.cmlterraform.name,
+  ]
+}
+
+#### Create policies
+
+# Create inline policy and attach it to  cml_terraform group
+# Policy that specifies that users in this group can pass the role s3-access-for-ec2
 resource "aws_iam_group_policy" "cmlterraform_inline_passrole" {
   name  = "pass-role"
   group = aws_iam_group.cmlterraform.name
@@ -66,7 +50,7 @@ resource "aws_iam_group_policy" "cmlterraform_inline_passrole" {
   })
 }
 
-# Policy which allows access to bucket
+# Customer managed policy which allows access to bucket
 # Will be attached to user group
 # It is also used as policy which is passed to EC2 instance so that EC2 instance has access rights to access S3 the same way as terraform user
 resource "aws_iam_policy" "s3_access_policy" {
@@ -97,17 +81,21 @@ resource "aws_iam_policy" "s3_access_policy" {
 }
 
 
-# import ARN for predefined AmazonEC2FullAccess policy
+# import ARN for predefined (AWS managed) AmazonEC2FullAccess policy
 # Provides full access to Amazon EC2 via the AWS Management Console.
 data "aws_iam_policy" "ec2fullaccess" {
   arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
-# import ARN for predefined AmazonEC2FullAccess policy
-# Provides full access to Amazon EC2 via the AWS Management Console.
+# import ARN for predefined (AWS managed) AmazonSSMManagedInstanceCore policy
+# Provides access to Amazon SSM service (used for EC2 instance)
 data "aws_iam_policy" "ssmaccess" {
   arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+
+#### Attach policies to cml_terraform group
+
 
 # Attach AmazonEC2FullAccess policy to cmlterraform group
 # User Group will have full access to EC2.
@@ -123,34 +111,10 @@ resource "aws_iam_group_policy_attachment" "cml_attach_cml_pol" {
   policy_arn = aws_iam_policy.s3_access_policy.arn
 }
 
-/*
-# This role will probably allow EC2 instance to access files in S3 bucket
-resource "aws_iam_role" "s3accessforec2" {
-  name = "s3-access-for-ec2"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
+#### Create role 
 
-resource "aws_iam_role_policy_attachment" "cmlterraformpolicy" {
-  role       = aws_iam_role.s3accessforec2.name
-  policy_arn = aws_iam_policy.s3_access_policy.arn
-}
-*/
-
-
+# Trust relationship for role s3-access-for-ec2
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -164,14 +128,14 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
-# This role will allow EC2 instance access files in S3 bucket
-# (on my behalf, see bellow policy attachment)
-# The policy, or maybe instance profile (bellow) will be associated with EC2 instance when is also used as policy wich is passed to EC2 instance so that EC2 instance has  created
+# Create role s3-access-for-ec2. The role can be assumed (used) by EC2 service only
+# EC2 instance will be able to access specific S3 bucket and SSM service
 resource "aws_iam_role" "s3accessforec2" {
   name               = "s3-access-for-ec2"
   path               = "/"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
 
 
 # Not sure what is also used as policy wich is passed to EC2 instance so that EC2 instance has  relationship between role and instance profile
@@ -181,33 +145,16 @@ resource "aws_iam_instance_profile" "s3accessforec2" {
   role = aws_iam_role.s3accessforec2.name
 }
 
-# Role will have access to S3 bucket on my behalf
+#### Attach policies to the role s3-access-for-ec2
+
+# Role s3-access-for-ec2 will have access to S3 bucket 
 resource "aws_iam_role_policy_attachment" "cmlterraformpolicy" {
   role       = aws_iam_role.s3accessforec2.name
   policy_arn = aws_iam_policy.s3_access_policy.arn
 }
 
-# Role will have access to SSM (on my behalf ?)
+# Role s3-access-for-ec2 will have access to SSM 
 resource "aws_iam_role_policy_attachment" "cmlterraformpolicy2" {
   role       = aws_iam_role.s3accessforec2.name
   policy_arn = data.aws_iam_policy.ssmaccess.arn
-}
-
-# Probably we will have to create access keys in GUI for the user ?
-resource "aws_iam_user" "cml_terraform" {
-  name = "cml_terraform"
-  path = "/"
-
-  tags = {
-    tag-key = "cml_terraform"
-  }
-}
-
-# associate the user with user group
-resource "aws_iam_user_group_membership" "cml_terraform" {
-  user = aws_iam_user.cml_terraform.name
-
-  groups = [
-    aws_iam_group.cmlterraform.name,
-  ]
 }
